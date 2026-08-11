02b5ed73031e5e45db422acf7454ae156739d7bd
FabricIntegration <gaddamrithvikfabric@gmail.com>
2026-08-11 08:10:30 +0530

    New and few updates for the existing Item Created.

---
+++ SelfLearning/EmpDataLoad.Notebook/notebook-content.py
@@ -0,0 +1,24 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+varEmpId = 101
+varEmpName = 'Test'
+print(varEmpId)
+print(varEmpName)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
