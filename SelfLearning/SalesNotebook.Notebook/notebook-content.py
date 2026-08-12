e8272b2a14120ec28d044d30c82f98b5684521b5
FabricIntegration <gaddamrithvikfabric@gmail.com>
2026-08-12 08:22:16 +0530

    LH Update

--- SelfLearning/SalesNotebook.Notebook/notebook-content.py
+++ SelfLearning/SalesNotebook.Notebook/notebook-content.py
@@ -8,16 +8,31 @@
 # META   },
 # META   "dependencies": {
 # META     "lakehouse": {
-# META       "default_lakehouse": "68bc2fa4-91ad-4e53-9052-929849932abf",
+# META       "default_lakehouse": "49932abf-9298-9052-4e53-91ad68bc2fa4",
 # META       "default_lakehouse_name": "FinanceLH",
-# META       "default_lakehouse_workspace_id": "affc0168-bb0d-43e9-84dd-c17e3f24502b",
+# META       "default_lakehouse_workspace_id": "00000000-0000-0000-0000-000000000000",
 # META       "known_lakehouses": [
 # META         {
-# META           "id": "68bc2fa4-91ad-4e53-9052-929849932abf"
+# META           "id": "49932abf-9298-9052-4e53-91ad68bc2fa4",
+# META           "workspace_id": "00000000-0000-0000-0000-000000000000"
 # META         }
 # META       ]
 # META     }
 # META   }
+# META }
+
+# CELL ********************
+
+varEmpId = 101
+varEmpName = 'Test'
+print(varEmpId)
+print(varEmpName)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
 # META }
 
 # CELL ********************
